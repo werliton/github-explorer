3d76d5d47bf29d247829f5818062922c7eaf1cf2
werliton <werliton.carlos@ceuma.br>
2020-10-05 17:56:11 -0300

    Iniciando estilizacao do dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import { Title, TitleContainer, Container, Form, Repositories } from './styles'
 
-const Dashboard:React.FC = () => {
+import logoImg from '../../assets/logo.svg'
+
+const Dashboard: React.FC = () => {
     return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
+        <Container>
+            <TitleContainer>
+                <img src={logoImg} alt="Github Explorer"/>
+                <Title>Explore repositórios no Github</Title>
+            </TitleContainer>
+            <Form>
+                <input placeholder="Digite aqui o nome do repositorio"/>
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            <Repositories>
+                <a href="re">
+                    <img src="https://avatars1.githubusercontent.com/u/4674324?s=460&u=cb676169391ac204b824569fd7465fa36488624d&v=4" alt="Perfil"/>
+                    <div>
+                        <strong>template-nodejs-with-solid</strong>
+                        <p>Projeto desenvolvido nas aulas da Rocketseat no goStack</p>
+                    </div>
+                </a>
+            </Repositories>
+        </Container>
     )
 }
 
---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,95 @@
+import styled from 'styled-components'
+import { shade } from 'polished'
+
+export const Container = styled.div`
+    max-width:700px;
+`
+export const Title = styled.h1`
+    font-size: 30px;
+    color: #3a3a3a;
+
+    max-width: 450px;
+    line-height: 56px;
+    margin-top: 80px;
+
+`
+
+export const TitleContainer = styled.div`
+    max-width:300px;
+`
+
+export const Form = styled.form`
+    margin-top: 40px;
+    max-width: 700px;
+
+    display: flex;
+
+    input {
+        flex:1;
+        height: 70px;
+        padding: 0 24px;
+        border: 0;
+        border-radius: 5px 0 0 5px;
+
+        &::placeholder{
+            color: #a8a8b3;
+        }
+        color: #3a3a3a;
+     }
+
+     button {
+         width: 210px;
+         height: 70px;
+         background: #04d361;
+         border-radius: 0px 5px 5px 0px;
+         border:0;
+         color: #fff;
+         font-weight: bold;
+         transition: background-color 0.2s;
+
+         &:hover{
+            background: ${shade(0.2, '#04d361')}
+         }
+     }
+`
+
+export const Repositories = styled.div`
+    margin-top: 80px;
+    max-width: 700px;
+
+    a{
+        background: #fff;
+        border-radius: 5px;
+        width: 100%;
+        padding: 14px;
+        display: block;
+        flex-direction: row;
+        align-items: center;
+
+        text-decoration:none;
+        display: flex;
+
+        img {
+            border-radius: 50%;
+            width: 10%;
+        }
+
+        div {
+            margin-left: 16px;
+
+            strong{
+                font-size: 20px;
+                color: #3D3D4D;
+            }
+
+            p {
+                font-size: 18px;
+                color: #a8a8b3;
+                margin-top: 4px;
+            }
+        }
+
+    }
+
+
+`
